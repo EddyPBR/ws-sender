dad131f528c458048c88a1a1964c4e0d086127c4
EddyPBR <edvaldojunior1310@gmail.com>
2021-10-25 16:35:16 -0300

    feat: enabling whatsapp client when server run

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,11 @@ import "dotenv/config";
 
 import { app } from "@src/app";
 
-app.listen(process.env.PORT, () => console.log("Server listen on Port:", process.env.PORT));
+import { client } from "@src/lib/whatsapp";
+
+app.listen(process.env.PORT, () => {
+  console.log("Server listen on Port:", process.env.PORT);
+  client.initialize()
+    .then(() => console.log("Whatsapp client is on!"))
+    .catch(() => console.log("Failed to init whatsapp client"))
+});
