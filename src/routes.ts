639dffb5dc4659929b390b96cf6382dca582e1a3
EddyPBR <edvaldojunior1310@gmail.com>
2021-10-25 16:35:59 -0300

    feat: create new route /send-message

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
 
+import { SendWhatsappMessageController } from "@src/controllers/SendWhatsappMessageController";
+
 const routes = Router();
 
-routes.get("/", (req, res) => {
-	return res.json({ message: "Hello World" });
-});
+routes.post("/send-message", new SendWhatsappMessageController().handle);
 
 export { routes };
