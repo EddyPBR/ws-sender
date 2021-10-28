4b03be25824cab39196dc21a6c1794922b10a432
EddyPBR <edvaldojunior1310@gmail.com>
2021-10-28 17:36:48 -0300

    feat(routes): [POST] /contact - create contact was added

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
 import { SendWhatsappMessageController } from "@src/controllers/SendWhatsappMessageController";
+import { CreateContactController } from "@src/controllers/CreateContactController";
 
 const routes = Router();
 
 routes.post("/send-message", new SendWhatsappMessageController().handle);
+routes.post("/contact", new CreateContactController().handle);
 
 export { routes };
