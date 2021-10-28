1805079ceda41569397f867daabf2f9c236fe01f
EddyPBR <edvaldojunior1310@gmail.com>
2021-10-28 17:36:06 -0300

    feat(service): create contact was created

---
+++ src/services/CreateContactService.ts
@@ -0,0 +1,27 @@
+import prismaClient from "@src/database/prisma";
+import { ApplicationException } from "@src/exceptions/ApplicationException";
+
+class CreateContactService {
+  async execute(name: string, phone: string) {
+    const exists = await prismaClient.phones.findUnique({
+      where: {
+        phone,
+      }
+    });
+
+    if(exists) {
+      throw new ApplicationException("Phone number already exists", 409);
+    }
+
+    const contact = await prismaClient.phones.create({
+      data: {
+        name, 
+        phone
+      }
+    });
+
+    return contact;
+  }
+}
+
+export { CreateContactService };
