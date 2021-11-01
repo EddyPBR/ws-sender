d771f12efea8e689965cfc55a6782c0acd52b0d1
EddyPBR <edvaldojunior1310@gmail.com>
2021-11-01 14:19:06 -0300

    feat(api): enabling cors policy

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,9 +1,14 @@
 import express from "express";
 import "express-async-errors";
 import { ErrorHandling } from "@src/middlewares/ErrorHandling";
+import cors from "cors";
 import { routes } from "@src/routes";
 
 const app = express();
+
+app.use(cors({
+  origin: "*",
+}));
 
 app.use(express.json());
 
