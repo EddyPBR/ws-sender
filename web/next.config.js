797e5ffb576a11e9fd38680d18369fdcecef983f
EddyPBR <edvaldojunior1310@gmail.com>
2021-11-01 10:57:53 -0300

    chore(web): configure pwa from nextjs

--- web/next.config.js
+++ web/next.config.js
@@ -1,4 +1,22 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-}
+const withPlugins = require("next-compose-plugins");
+const withPWA = require("next-pwa");
+
+module.exports = withPlugins([
+  {
+    images: {
+      domains: ["lh3.googleusercontent.com"],
+    },
+  },
+  [
+    withPWA,
+    {
+      pwa: {
+        disable: process.env.NODE_ENV !== "production",
+        register: true,
+        scope: "/",
+        dest: "public",
+        sw: "/service-worker.js",
+      },
+    },
+  ],
+]);
