70d92f16365a91d683fcd84c5a12a78f125592d0
EddyPBR <edvaldojunior1310@gmail.com>
2021-11-03 14:57:47 -0300

    feat(web): home page restore session from localstorage & redirect

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,6 +1,8 @@
 import type { NextPage } from "next";
 import QRCode from "react-qr-code";
 import { useState, useEffect } from "react";
+import io from "socket.io-client";
+import { useRouter } from "next/router";
 
 import { Load } from "../components/Load";
 
@@ -8,28 +10,69 @@ import { Container, QRCodeBox } from "../styles/global";
 
 import { api } from "../services/api";
 
+interface IWhatsAppSession {
+  WABrowserId: string;
+  WASecretBundle: string;
+  WAToken1: string;
+  WAToken2: string;
+}
+
 const Home: NextPage = () => {
-  const [QRCodeSession, setQRCodeSession] = useState<string | undefined>();
+  const [qrCodeSession, setQrCodeSession] = useState<string | undefined>();
+  const [session, setSession] = useState<IWhatsAppSession | undefined>();
+
+  const router = useRouter();
 
   useEffect(() => {
     const getQRCode = async () => {
       try {
         const response = await api.get("qrcode");
-        const { QRCode } = response.data;
-        setQRCodeSession(QRCode);
+        const { qrCode } = response.data;
+        setQrCodeSession(qrCode);
       } catch (err) {
         console.log(err);
       }
     }
 
-    getQRCode();
+    const checkWhatsappSession = async (session: IWhatsAppSession) => {
+      try {
+        await api.post("session", session);
+
+        setSession(session);
+        router.push("/dashboard");
+      } catch (err) {
+        setSession(undefined);
+        localStorage.removeItem("@ws-sender:session");
+        console.log(err);
+      }
+    }
+
+    const session = localStorage.getItem("@ws-sender:session");
+
+    if(session) {
+      checkWhatsappSession(JSON.parse(session));
+    } else {
+      getQRCode();
+    }
+  }, []);
+
+  useEffect(() => {
+    const socket = io(process.env.NEXT_PUBLIC_API_URL || "");
+
+    socket.on("new_connection", (session: IWhatsAppSession) => {
+      const sessionString = JSON.stringify(session);
+      localStorage.setItem("@ws-sender:session", sessionString);
+      router.push("/dashboard");
+    });
   }, []);
 
   return (
     <Container>
-      {QRCodeSession ? (
+      { session && <h1>AUTENTICADO</h1> }
+
+      {qrCodeSession && !session ? (
         <QRCodeBox>
-          <QRCode value={QRCodeSession} />
+          <QRCode value={qrCodeSession} />
         </QRCodeBox>
       ) : (
         <Load size={32} />
