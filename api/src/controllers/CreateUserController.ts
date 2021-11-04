b89e507fa130ce2db3f182087b04fa98f6b0d5da
EddyPBR <edvaldojunior1310@gmail.com>
2021-11-04 15:11:45 -0300

    feat(api): create user controller

---
+++ api/src/controllers/CreateUserController.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+import { CreateUserService } from "@services/CreateUserService";
+
+interface ICreateUserRequest {
+  email: string;
+  password: string;
+}
+
+class CreateContactController {
+  async handle(request: Request, response: Response) {
+    const { email, password }: ICreateUserRequest = request.body;
+
+    const service = new CreateUserService();
+
+    const result = await service.execute(email, password);
+
+    return response.status(200).json({
+      result
+    });
+  }
+}
+
+export { CreateContactController };
