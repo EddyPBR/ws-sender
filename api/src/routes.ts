5c210d1d4922e4e39cd59ccedda548e5be605bb4
EddyPBR <edvaldojunior1310@gmail.com>
2021-11-04 16:24:02 -0300

    feat(api): create route [POST] /authenticate - to authenticate users

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { celebrate, Joi } from "celebrate";
 
 import { CreateUserController } from "@controllers/CreateUserController";
+import { AuthenticateUserController } from "@controllers/AuthenticateUserController";
 
 const routes = Router();
 
@@ -11,5 +12,12 @@ routes.post("/user", celebrate({
     password: Joi.string().min(6).max(18).required()
   }
 }), new CreateUserController().handle);
+
+routes.post("/authenticate", celebrate({
+  body: {
+    email: Joi.string().lowercase().email().required(),
+    password: Joi.string().required()
+  }
+}), new AuthenticateUserController().handle);
 
 export { routes };
