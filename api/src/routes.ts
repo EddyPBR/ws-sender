440fab59f0fd96b751b7a435e78c934c7b22b40e
EddyPBR <edvaldojunior1310@gmail.com>
2021-11-04 15:11:57 -0300

    feat(api): create route [POST] /user - to create user

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,36 +1,15 @@
 import { Router } from "express";
-// import { celebrate, Joi } from "celebrate";
+import { celebrate, Joi } from "celebrate";
 
-// import { SendWhatsappMessageController } from "@src/controllers/SendWhatsappMessageController";
-// import { CreateContactController } from "@src/controllers/CreateContactController";
-// import { CreateQRCodeController } from "@src/controllers/CreateQRCodeController";
-// import { SetWhatsappSessionController } from "@src/controllers/SetWhatsappSessionController";
+import { CreateContactController } from "@controllersCreateUserController";
 
 const routes = Router();
 
-// routes.post("/send-message", celebrate({
-// 	body: {
-// 		phone: Joi.string().required().regex(/^\([1-9]{2}\) (?:[2-8]|9[1-9])[0-9]{3}-[0-9]{4}$/),
-// 		message: Joi.string().min(1).max(65500).required()
-// 	}
-// }), new SendWhatsappMessageController().handle);
-
-// routes.post("/contact", celebrate({
-// 	body: {
-// 		name: Joi.string().min(1).max(190).required(),
-// 		phone: Joi.string().required().regex(/^\([1-9]{2}\) (?:[2-8]|9[1-9])[0-9]{3}-[0-9]{4}$/),
-// 	}
-// }), new CreateContactController().handle);
-
-// routes.get("/qrcode", new CreateQRCodeController().handle);
-
-// routes.post("/session", celebrate({
-// 	body: {
-// 		WABrowserId: Joi.string().required(),
-// 		WASecretBundle: Joi.string().required(),
-// 		WAToken1: Joi.string().required(),
-// 		WAToken2: Joi.string().required()
-// 	}
-// }), new SetWhatsappSessionController().handle);
+routes.post("/user", celebrate({
+  body: {
+    email: Joi.string().lowercase().email().required(),
+    password: Joi.string().min(6).max(18).required()
+  }
+}), new CreateContactController().handle);
 
 export { routes };
