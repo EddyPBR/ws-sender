089f1660606a4899e6ef23346d2133cfd77bb320
EddyPBR <edvaldojunior1310@gmail.com>
2021-11-08 14:53:48 -0300

    feat(web): form prepared to integration with api

--- web/src/pages/login/index.tsx
+++ web/src/pages/login/index.tsx
@@ -2,13 +2,25 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
+import { useForm } from "react-hook-form";
 import { AiOutlineMail } from "react-icons/ai";
 import { BsShieldLock } from "react-icons/bs";
 import { Backlink } from "@components/Backlink";
 
 import { Content, Box, LoginForm, OtherSection } from "./styles";
 
+interface ILoginFormValues {
+  email: string;
+  password: string;
+}
+
 const Login: NextPage = () => {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
+  async function handleLogin({ email, password }: ILoginFormValues) {
+    console.log(email, password);
+  }
+
   return (
     <>
       <Head>
@@ -31,15 +43,27 @@ const Login: NextPage = () => {
             <h1>Login</h1>
             <p>Faça login para acessar o sistema</p>
 
-            <LoginForm>
-              <label htmlFor="email" className="input">
+            <LoginForm onSubmit={handleSubmit(handleLogin)}>
+              <label htmlFor="email" className={`${errors?.email ? "input error" : "input"}`} >
                 <AiOutlineMail />
-                <input id="email" type="email" placeholder="seuemail@exemplo.com" title="email" name="email" />
+                <input 
+                  id="email"
+                  type="email"
+                  placeholder="seuemail@exemplo.com"
+                  title="Seu email"
+                  {...register("email", {required: true, pattern: /^\S+@\S+$/i})}
+                />
               </label>
 
-              <label htmlFor="password" className="input">
+              <label htmlFor="password" className={`${errors?.password ? "input error" : "input"}`}>
                 <BsShieldLock />
-                <input id="password" type="password" placeholder="sua senha" title="senha" name="password" />
+                <input 
+                  id="password" 
+                  type="password" 
+                  placeholder="Sua senha" 
+                  title="senha" 
+                  {...register("password", {required: true, minLength: 6, maxLength: 18})}
+                />
               </label>
 
               <button type="submit">Login</button>
