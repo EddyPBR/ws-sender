dabfa5de344af64ed6b97dfb872e32d242abed22
EddyPBR <edvaldojunior1310@gmail.com>
2021-11-08 15:08:55 -0300

    feat(web): page /registrar - prepared form to integrate with api

--- web/src/pages/registrar/index.tsx
+++ web/src/pages/registrar/index.tsx
@@ -2,13 +2,30 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
+import { useForm } from "react-hook-form";
 import { AiOutlineMail } from "react-icons/ai";
 import { BsShieldLock } from "react-icons/bs";
 import { Backlink } from "@components/Backlink";
 
-import { Content, Box, LoginForm, OtherSection } from "./styles";
+import { Content, Box, RegisterForm, OtherSection } from "./styles";
+
+interface IRegisterFormValues {
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
 
 const Registrar: NextPage = () => {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
+  async function handleRegisterPassword({ email, password, confirmPassword }: IRegisterFormValues) {
+    if(password !== confirmPassword) {
+      console.log("As senhas não estão iguais!")
+    }
+
+    console.log(email);
+  }
+
   return (
     <>
       <Head>
@@ -31,24 +48,42 @@ const Registrar: NextPage = () => {
             <h1>Registrar</h1>
             <p>Informe seu email e crie uma senha</p>
 
-            <LoginForm>
-              <label htmlFor="email" className="input">
+            <RegisterForm onSubmit={handleSubmit(handleRegisterPassword)}>
+              <label htmlFor="email" className={`${errors?.email ? "input error" : "input"}`}>
                 <AiOutlineMail />
-                <input id="email" type="email" placeholder="seuemail@exemplo.com" title="email" name="email" />
+                <input 
+                  id="email" 
+                  type="email" 
+                  placeholder="seuemail@exemplo.com" 
+                  title="email"
+                  {...register("email", {required: true, pattern: /^\S+@\S+$/i})}
+                />
               </label>
 
-              <label htmlFor="password" className="input">
+              <label htmlFor="password" className={`${errors?.email ? "input error" : "input"}`}>
                 <BsShieldLock />
-                <input id="password" type="password" placeholder="sua senha" title="senha" name="password" />
+                <input 
+                  id="password" 
+                  type="password" 
+                  placeholder="sua senha" 
+                  title="senha"
+                  {...register("password", { required: true, minLength: 6, maxLength: 18 })}
+                />
               </label>
 
-              <label htmlFor="confirmPassword" className="input">
+              <label htmlFor="confirmPassword" className={`${errors?.email ? "input error" : "input"}`}>
                 <BsShieldLock />
-                <input id="confirmPassword" type="confirmPassword" placeholder="confirme sua senha" title="confirmar senha" name="confirmPassword" />
+                <input 
+                  id="confirmPassword" 
+                  type="confirmPassword" 
+                  placeholder="confirme sua senha" 
+                  title="confirmar senha"
+                  {...register("confirmPassword", { required: true, minLength: 6, maxLength: 18 })}
+                />
               </label>
 
-              <button type="submit">Login</button>
-            </LoginForm>
+              <button type="submit">Cadastrar</button>
+            </RegisterForm>
           </Box>
 
           <OtherSection>
