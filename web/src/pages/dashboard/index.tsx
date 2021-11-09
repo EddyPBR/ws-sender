4cb44a328a3e10dd5c2a4fb2f6d5b4bd7b201968
EddyPBR <edvaldojunior1310@gmail.com>
2021-11-09 14:49:58 -0300

    feat(web): create section when start instance

--- web/src/pages/dashboard/index.tsx
+++ web/src/pages/dashboard/index.tsx
@@ -1,11 +1,13 @@
 import type { NextPage, GetServerSideProps, NextPageContext } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import { useState } from "react";
 import { parseCookies, destroyCookie } from "nookies";
 import { Navbar } from "@components/Navbar";
+import { QrCode } from "@components/QrCode";
 import { api } from "@services/api";
 
-import { Presentation } from "./styles";
+import { Presentation, Tutorial } from "./styles";
 
 interface IUserData {
   email: string;
@@ -15,7 +17,11 @@ interface IDashboardPageContext extends NextPageContext {
   user: IUserData;
 }
 
+type DashboardStatusType = "not initialized" | "initialized" | "in session";
+
 const Dashboard: NextPage<IDashboardPageContext> = ({ user }) => {
+  const [status, setStatus] = useState<DashboardStatusType>("not initialized");
+
   return (
     <>
       <Head>
@@ -25,37 +31,64 @@ const Dashboard: NextPage<IDashboardPageContext> = ({ user }) => {
       <Navbar onDashboard />
 
       <div className="container mini">
-        <Presentation>
-          <article>
-            <h1>
-              Olá,
-              <br />
-              <strong>
-                {user?.email}
-              </strong>
-            </h1>
+        {(status === "not initialized") && (
+          <Presentation>
+            <article>
+              <h1>
+                Olá,
+                <br />
+                <strong>
+                  {user?.email}
+                </strong>
+              </h1>
 
-            <p>
-              Seja bem vindo ao <b>WAS</b>, para iniciar basta clicar no botão iniciar 
-              conexão, assim vamos construir uma nova instância para o seu 
-              WhatsApp.
-            </p>
+              <p>
+                Seja bem vindo ao <b>WAS</b>, para iniciar basta clicar no botão iniciar 
+                conexão, assim vamos construir uma nova instância para o seu 
+                WhatsApp.
+              </p>
 
-            <button type="button">
-              Iniciar
-            </button>
-          </article>
+              <button type="button" onClick={() => setStatus("initialized")}>
+                Iniciar
+              </button>
+            </article>
 
-          <article>
-            <Image 
-              src="/assets/connect.svg" 
-              width="500"
-               height="384" 
-               alt="Inicie a instância e vamos começar!" 
-               priority={true} 
-            />
-          </article>
-        </Presentation>
+            <article>
+              <Image 
+                src="/assets/connect.svg" 
+                width="500"
+                height="384" 
+                alt="Inicie a instância e vamos começar!" 
+                priority={true} 
+              />
+            </article>
+          </Presentation>
+        )}
+        {(status === "initialized") && (
+          <Tutorial>
+            <article>
+              <p>
+                Para utilizar o WAS precisamos que você conecte o seu WhatsApp, o 
+                procecimento é bem simples, abra o aplicativo em seu smartphone e 
+                siga o exemplo abaixo.
+              </p>
+              <Image
+                src="/assets/was-qrcode.png"
+                alt="Para conectar o WAS ao seu WhatsApp, abra o aplicativo, 
+                precione no ícone de três pontinhos (opções), depois selecione 
+                'aparelhos conectados' e finalmente pressione o botão 
+                'Conectar aparelho', faça o escaneamento do QR Code e pronto!"
+                width="470"
+                height="220"
+                priority={true}
+              />
+              <p>
+                Com tudo feito, basta escanear o QR Code que fornecemos.
+              </p>
+            </article>
+            <QrCode />
+          </Tutorial>
+        )}
       </div>
     </>
   )
--- web/src/pages/dashboard/styles.ts
+++ web/src/pages/dashboard/styles.ts
@@ -49,3 +49,18 @@ export const Presentation = styled.section`
     }
   }
 `;
+
+export const Tutorial = styled.section`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+
+  article {
+    display: flex;
+    flex-direction: column;
+    gap: 2.8rem;
+    max-width: 47rem;
+  }
+`;
