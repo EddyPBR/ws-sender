d29a99db561121158b0f361d2ad7d041755b7630
EddyPBR <edvaldojunior1310@gmail.com>
2021-11-10 14:39:37 -0300

    feat(web): enabling WhatsAppProvider in the project

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -3,15 +3,21 @@ import { theme } from "@styles/theme";
 import { GlobalStyle } from "@styles/global";
 import { Toaster } from "react-hot-toast";
 import { toastStyles } from "@styles/toast";
+import { WhatsAppProvider } from "@contexts/WhatsAppContext";
 
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
-      <Toaster position="top-right" toastOptions={toastStyles} />
-      <Component {...pageProps} />
       <GlobalStyle />
+      <Toaster 
+        position="top-right" 
+        toastOptions={toastStyles} 
+      />
+      <WhatsAppProvider>
+        <Component {...pageProps} />
+      </WhatsAppProvider>
     </ThemeProvider>
   );
 }
