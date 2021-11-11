c8f39d351c82a7adc62795dde8deb6e984b85e58
EddyPBR <edvaldojunior1310@gmail.com>
2021-11-11 10:21:41 -0300

    feat(web): creating styles for scrollbar

--- web/src/styles/global.ts
+++ web/src/styles/global.ts
@@ -5,6 +5,21 @@ export const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+
+    &::-webkit-scrollbar {
+      width: 0.4rem;
+      height: 0.4rem;
+    }
+
+    &::-webkit-scrollbar-track-piece {
+      background-color: ${props => props.theme.colors.background};
+    }
+
+    &::-webkit-scrollbar-thumb {
+      border: 0.2rem solid ${props => props.theme.colors.green1};
+      background-clip: padding-box;
+      border-radius: 9999px;
+    }
   }
 
   html {
