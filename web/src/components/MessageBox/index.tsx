88ffceff4071bc97b10727d9e7d28b4477319735
EddyPBR <edvaldojunior1310@gmail.com>
2021-11-12 11:00:34 -0300

    feat(web): component message box create function to send many messages

--- web/src/components/MessageBox/index.tsx
+++ web/src/components/MessageBox/index.tsx
@@ -1,4 +1,5 @@
 import { FaTelegramPlane } from "react-icons/fa";
+
 import {
   MessageForm,
   TextArea,
@@ -7,14 +8,61 @@ import {
   SendMessageButton
 } from "./styles";
 
+import { useWhatsApp } from "@hooks/useWhatsApp";
+import { Load } from "@components/Load";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+import toast from "react-hot-toast";
+
+interface ISendManyMessageFormValues {
+  message: string;
+}
+
 export function MessageBox() {
+  const { handleSendMessage, isSendingMessage } = useWhatsApp();
+  const { register, handleSubmit } = useForm();
+
+  const [phones, setPhones] = useState<string[]>([
+    "5583987956936",
+    "5583987956936",
+    "5583987956936"
+  ]);
+
+  async function handleSendManyMessages({ message }: ISendManyMessageFormValues) {
+    if (!message || phones.length === 0) {
+      toast.error("Necessário informar mensagem e pelo menos um contato");
+      return;
+    }
+
+    const sendManyMessagesPromise = Promise.all(
+      phones.map(async (phone) => await handleSendMessage(message, phone))
+    );
+
+    toast.promise(
+      sendManyMessagesPromise,
+      {
+        loading: `Enviando mensagens...`,
+        success: () => `Mensagens foram enviadas`,
+        error: () => `Falha ao enviar mensagens.`,
+      },
+      {
+        loading: {
+          icon: '💬'
+        },
+        success: {
+          duration: 6000,
+        },
+      }
+    );
+  }
+
   return (
-    <MessageForm>
+    <MessageForm onSubmit={handleSubmit(handleSendManyMessages)}>
       <TextArea
         id="message"
-        name="message"
-        title="Sua mensagem"
         placeholder="Escreva sua mensagem aqui"
+        title="Sua mensagem"
+        {...register("message", { required: true })}
       />
 
       <ControllBox>
@@ -22,7 +70,10 @@ export function MessageBox() {
         </SelectedContactsBox>
 
         <SendMessageButton type="submit">
-          <FaTelegramPlane size={18} />
+          {isSendingMessage
+            ? <Load size={20} />
+            : <FaTelegramPlane size={18} />
+          }
         </SendMessageButton>
       </ControllBox>
     </MessageForm>
